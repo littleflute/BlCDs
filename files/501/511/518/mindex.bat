2d49449d815225eadbe2ca33aa7c4b76101c3eef
littleflute <13699175164@sina.cn>
2017-03-27 15:35:08 -0600

    Add files via upload

---
+++ files/501/511/518/mindex.bat
@@ -0,0 +1,17 @@
+;v0.0.1
+ 
+del index.html
+echo ^<html^>	>> 	index.html
+echo ^<head^>	>> 	index.html
+echo ^<title^>	>> 	index.html
+ Creat html by Bat 
+echo ^</title^>	>> 	index.html
+echo ^</head^>	>> 	index.html
+echo ^<body^>	>> 	index.html
+
+echo ^<a href="http://www.beautifullover.org" target="_blank"^>	>> 	index.html
+echo beautifullover ^</a^>	>>	index.html
+
+
+echo ^</body^>	>> 	index.html
+echo ^</html^>	>> 	index.html
