6387c4a6dc019a19efbbefb9d9a560e5304f95c3
littleflute <13699175164@sina.cn>
2017-03-27 14:56:55 -0600

    Add files via upload

---
+++ files/501/511/517/xdcmd.bat
@@ -0,0 +1,22 @@
+;v0.0.2
+
+del index.html
+
+findstr "<html>" 	1.txt	>> 	index.html
+findstr "<head>" 	1.txt	>> 	index.html
+findstr "<title>" 	1.txt 	>> 	index.html
+echo xdcmd2html Test		>>	index.html
+findstr "</title>" 	1.txt 	>> 	index.html
+findstr "</head>" 	1.txt 	>> 	index.html
+findstr "<body>" 	1.txt 	>> 	index.html
+
+
+echo xdcmd2html Test;		>>	index.html
+
+echo xdcmd2html Test;		>>	index.html
+echo xdcmd2html Test;		>>	index.html
+
+
+findstr "</body>" 	1.txt 	>> 	index.html
+findstr "</html>" 	1.txt 	>> 	index.html
+ 
