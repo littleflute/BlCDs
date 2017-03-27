fae3b11151cded79dc6ed86bd6418c620ffb05aa
littleflute <13699175164@sina.cn>
2017-03-27 16:08:29 -0600

    Add files via upload

---
+++ files/501/511/520/mindex.bat
@@ -0,0 +1,21 @@
+;v0.0.1
+ 
+del index.html
+echo ^<html^>	>> 	index.html
+echo ^<head^>	>> 	index.html
+echo ^<title^>	>> 	index.html
+ Creat html by Bat 
+echo ^</title^>	>> 	index.html
+echo ^</head^>	>> 	index.html
+echo ^<body^>	>> 	index.html
+
+echo ^<a href="http://www.beautifullover.org" target="_blank"^>	>> 	index.html
+echo beautifullover ^</a^>	>>	index.html
+echo ^</br^>	>>index.html
+
+echo ^<a href="https://github.com/littleflute/BlCDs/issues/519" target="_blank"^>	>> 	index.html
+echo issue519 ^</a^>	>>	index.html
+
+
+echo ^</body^>	>> 	index.html
+echo ^</html^>	>> 	index.html
